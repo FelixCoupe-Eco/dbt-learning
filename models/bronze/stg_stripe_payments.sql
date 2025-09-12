c80c67d12beffd75405169d911ba410350bf8a07
FelixCoupe-Eco <felix.coupe@ecoonline.com>
2025-09-12 15:53:26 +0200

    deleted leftover files in originla repo, and started reactoring process

--- models/bronze/stg_stripe_payments.sql
+++
@@ -1,8 +0,0 @@
-select 
-    id as customer_id,
-    orderid as order_id, 
-    paymentmethod as payment_method, 
-    status as order_status, 
-    amount / 100 as order_amount, 
-    created as created_at
-from {{ source('stripe', 'payment') }}
