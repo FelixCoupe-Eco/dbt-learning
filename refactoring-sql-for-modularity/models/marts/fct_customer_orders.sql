69c5e60808d6d95dc047b01328f2839f162e0064
FelixCoupe-Eco <felix.coupe@ecoonline.com>
2025-09-12 16:49:38 +0200

    Cleaned up and compartmentalized fct model into ctes

--- refactoring-sql-for-modularity/models/marts/fct_customer_orders.sql
+++ refactoring-sql-for-modularity/models/marts/fct_customer_orders.sql
@@ -1,64 +1,66 @@
--- Original customer_orders model (non-refactored)
+-- refactoring of customer_orders_legacy
 with
+    -- identify order ids with successful payments
+    successful_payments as (
+        select
+            order_id,
+            max(created) as payment_finalized_date,
+            sum(payment_amount) / 100.0 as total_amount_paid
+        from {{ ref("stg_payments") }}
+        where payment_status <> 'fail'
+        group by order_id
+    ),
+    -- collect order/customer information for successful orders
     paid_orders as (
         select
             orders.order_id,
             orders.customer_id,
             orders.order_date,
             orders.order_status,
-            payments.total_amount_paid,
-            payments.payment_finalized_date,
+            successful_payments.total_amount_paid,
+            successful_payments.payment_finalized_date,
             customers.first_name as customer_first_name,
             customers.last_name as customer_last_name
         from {{ ref("stg_orders") }} as orders
-        left join
-            (
-                select
-                    order_id,
-                    max(created) as payment_finalized_date,
-                    sum(payment_amount) / 100.0 as total_amount_paid
-                from {{ ref("stg_payments") }}
-                where payment_status <> 'fail'
-                group by 1
-            ) payments
-            on orders.order_id = payments.order_id
+        left join successful_payments on orders.order_id = successful_payments.order_id
         left join {{ ref("stg_customers") }} customers on orders.customer_id = customers.customer_id
     ),
-
-    customer_orders as (
+    -- Information about customer orders
+    customer_order_info as (
         select
             customers.customer_id,
-            min(order_date) as first_order_date,
-            max(order_date) as most_recent_order_date,
+            min(orders.order_date) as first_order_date,
+            max(orders.order_date) as most_recent_order_date,
             count(orders.order_id) as number_of_orders
         from {{ ref("stg_customers") }} customers
         left join {{ ref("stg_orders") }} as orders on orders.customer_id = customers.customer_id
         group by 1
-    )
-
-select
-    p.*,
-    row_number() over (order by p.order_id) as transaction_seq,
-    row_number() over (
-        partition by customer_id order by p.order_id
-    ) as customer_sales_seq,
-    case
-        when c.first_order_date = p.order_date then 'new' else 'return'
-    end as nvsr,
-    x.clv_bad as customer_lifetime_value,
-    c.first_order_date as fdos
-from paid_orders p
-left join customer_orders as c using (customer_id)
-left outer join
-    (
-        select p.order_id, sum(t2.total_amount_paid) as clv_bad
-        from paid_orders p
-        left join
-            paid_orders t2
-            on p.customer_id = t2.customer_id
-            and p.order_id >= t2.order_id
-        group by 1
-        order by p.order_id
-    ) x
-    on x.order_id = p.order_id
-order by order_id
+    ),
+    -- Total value of customer orders to date
+    customer_value as (
+        select 
+            order_id, 
+            customer_id,
+            total_amount_paid,
+            sum(total_amount_paid) over (
+                    partition by customer_id 
+                    order by order_id
+                ) as customer_lifetime_value
+        from paid_orders 
+    ),
+final as (
+    select
+        paid_orders.*,
+        row_number() over (order by paid_orders.order_id) as transaction_seq, -- to understand the sequence of orders
+        case
+            when customer_order_info.first_order_date = paid_orders.order_date then 'new' 
+            else 'returning'
+        end as new_or_returning_customer,
+        customer_value.customer_lifetime_value,
+        customer_order_info.first_order_date as customer_first_order_date
+    from paid_orders
+    left join customer_order_info using (customer_id)
+    left outer join customer_value on customer_value.order_id = paid_orders.order_id
+    order by order_id
+)
+select * from final
